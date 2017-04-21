277b20214545bce95573621ba39252fa079311b9
alvinmilagrosa <alvin.milagrosa1990@gmail.com>
2017-04-21 11:58:27 +0800

    first commit

---
+++ CounterIncreaser/App/MyApp.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    var app = angular.module('MyApp', ['ngRoute']);
+    app.controller('HomeController', function ($scope, api) {
+        $scope.currentValue = 1;
+        $scope.Increment = function (currentValue) {
+            if ($scope.currentValue >= 10) {
+                alert("Cannot be increment!");
+                api.GetTransactionWithParam(currentValue, 0, "Failed");
+            }
+            else {
+                $scope.currentValue = currentValue + 1;
+                api.GetTransactionWithParam(currentValue, $scope.currentValue, "Success");
+            }
+
+
+        }
+
+    })
+    .factory('api', function ($http) {
+        var fac = {};
+        fac.GetTransactionWithParam = function (currentNumber, numberAfterAdded, status) {
+            return $http.post('/Data/AddToCounterInformation', { CurrentNumber: currentNumber, NumberAfterAdded: numberAfterAdded, Status: status });
+        }
+        return fac;
+    });
+})();
---
+++ CounterIncreaser/Controllers/DataController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CounterIncreaser.Controllers
+{
+    public class DataController : Controller
+    {
+        [HttpPost]
+        public JsonResult AddToCounterInformation(int currentNumber, int numberAfterAdded, string status)
+        {
+            var result = 0;
+            string message = "";
+
+            using (CounterIncreaserDBEntities ct = new CounterIncreaserDBEntities())
+            {
+                var parameters = new SqlParameter[]
+                {
+                        new SqlParameter("@CurrentNumber", currentNumber),
+                        new SqlParameter("@NumberAfterAdded", numberAfterAdded),
+                         new SqlParameter("@Status", status)
+                };
+
+                result = ct.Database.ExecuteSqlCommand("spIncrementLog @CurrentNumber,@NumberAfterAdded,@Status", parameters);
+                if (result > 0)
+                {
+                    message = "Success";
+                }
+                else
+                {
+                    message = "error";
+                }
+            }
+            return new JsonResult { Data = message, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+    }
+}
